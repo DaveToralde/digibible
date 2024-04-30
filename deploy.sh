80df4df07ab6f869c9095940b19d6a79597c6224
iamdeibuu <98869054+iamdeibuu@users.noreply.github.com>
2024-04-30 23:11:50 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+
+export NODE_OPTIONS=--openssl-legacy-provider
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:ryben/verse.git main:gh-pages
+
+unset NODE_OPTIONS
+
+cd -
